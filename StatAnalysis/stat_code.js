7433ac08bf65f2ea436a017e165cfbdb033efc97
Pol Baladas Luna <polbaladasluna@gmail.com>
2015-06-13 12:55:42 +0000

    Started form design and importing of external data

--- StatAnalysis/stat_code.js
+++ StatAnalysis/stat_code.js
@@ -1,14 +1,23 @@
-
 function doGet(e) {
   var template = HtmlService.createTemplateFromFile('Form.html');
   template.action = ScriptApp.getService().getUrl();
+  template.escoles SpreadsheetApp
+    .openById('1GM9lxR7fS0u9bOvrB2RXlhAW_H76Xx6R9h6ABFVHvuc')
+    .getActiveSheet()
+    .getDataRange()
+    .getValues();
   return template.evaluate();
 }
 
-
-function processForm(theForm) {
+function processForm(form) {
   // Fill in response template
-  var template = HtmlService.createTemplateFromFile('result.html');
-  var name = template.name = theForm.name;
+  var template = HtmlService.createTemplateFromFile('Result.html');
+  var keys = getAnalytics(form);
   return template.evaluate().getContent();
+}
+
+
+function getAnalytics(form){
+  var keys = [];
+  return keys;
 }
