93141f43fc66676d1625fef9c058b4d07d0cb09c
Pol Baladas Luna <polbaladasluna@gmail.com>
2015-06-14 19:36:38 +0000

    Refactored stat_code.js : Added logs and getEscoles() function

--- StatAnalysis/stat_code.js
+++ StatAnalysis/stat_code.js
@@ -1,11 +1,7 @@
 function doGet(e) {
   var template = HtmlService.createTemplateFromFile('Form.html');
   template.action = ScriptApp.getService().getUrl();
-  template.escoles SpreadsheetApp
-    .openById('1GM9lxR7fS0u9bOvrB2RXlhAW_H76Xx6R9h6ABFVHvuc')
-    .getActiveSheet()
-    .getDataRange()
-    .getValues();
+  template.escoles = getEscoles();
   return template.evaluate();
 }
 
@@ -16,8 +12,24 @@ function processForm(form) {
   return template.evaluate().getContent();
 }
 
-
 function getAnalytics(form){
   var keys = [];
+  logForm(form);
   return keys;
+}
+
+function logForm(form){
+  var escoles = getEscoles();
+  Logger.log(form.year);
+  Logger.log(form.level);
+  Logger.log(form.question);
+  Logger.log(escoles[form.escola])
+}
+
+function getEscoles(){
+  return SpreadsheetApp
+    .openById('1GM9lxR7fS0u9bOvrB2RXlhAW_H76Xx6R9h6ABFVHvuc')
+    .getActiveSheet()
+    .getDataRange()
+    .getValues();
 }
