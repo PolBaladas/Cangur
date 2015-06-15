c058e56148de98300e3bdaeb13d9476db3d2b33e
Pol Baladas Luna <polbaladasluna@gmail.com>
2015-06-15 07:29:09 +0000

    Added question/answer table

--- StatAnalysis/stat_code.js
+++ StatAnalysis/stat_code.js
@@ -9,12 +9,16 @@ function processForm(form) {
   // Fill in response template
   var template = HtmlService.createTemplateFromFile('Result.html');
   var keys = getAnalytics(form);
+  template.keys = keys;
   return template.evaluate().getContent();
 }
 
 function getAnalytics(form){
   var keys = [];
   logForm(form);
+  var answers = getAnswers(parseInt(form.year), parseInt(form.level));
+  Logger.log(answers);
+  keys[0] = answers;
   return keys;
 }
 
@@ -25,6 +29,39 @@ function logForm(form){
   Logger.log(form.question);
   Logger.log(escoles[form.escola])
 }
+
+function getAnswers(year,level){
+  var ss = SpreadsheetApp.openById('1hnh4O4GiWQH8yJtppSdyy3SaVNXDZAx5YX9o-0Jc5AE');
+  var keySheetName = ss.getSheets()[1].getName();
+  var sheet = ss.setActiveSheet(ss.getSheetByName(keySheetName).activate());
+
+  var firstkeyRow = getFirstKeyRow(year,level,sheet)
+  var key = sheet.getSheetValues(firstkeyRow, 3, 1, 30)[0].valueOf();
+  return key;
+}
+
+function getFirstKeyRow(year, level, sheet){
+  var yearRow = getYearRow(year,sheet);
+  var lvlRow = getLevelRow(level, yearRow);
+  return lvlRow;
+}
+
+function getYearRow(requestedYear, sheet){
+  var rowNum = 1;
+  var foundYear = sheet.getRange("A2").getValue;
+
+  while(foundYear!=requestedYear){
+    foundYear = sheet.getRange("A"+rowNum).getValue();
+    rowNum+=1
+  }
+  return rowNum-1;
+}
+
+function getLevelRow(requestedLevel, yearRow){
+  var lvlRow = yearRow+requestedLevel;
+  return lvlRow-1;
+}
+
 
 function getEscoles(){
   return SpreadsheetApp
